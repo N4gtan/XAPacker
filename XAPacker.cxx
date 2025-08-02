bd9787e750c6ddbcd573ab639278f3f379b00e67
Nagtan <bkragnarok@gmail.com>
2025-08-02 00:48:00 -0300

    refactor: use XAPheader structure instead of a buffer little improvement to I/O

--- XAPacker.cxx
+++ XAPacker.cxx
@@ -4,6 +4,40 @@
 #define CD_SECTOR_SIZE 2352
 #define XA_DATA_SIZE 2336
 
+struct XAPheader
+{
+    unsigned char sync[12]   {0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00};
+    unsigned char addr[3];
+    unsigned char mode       {0x02};
+    unsigned char subHead[8] {0x01, 0x00, 0x48, 0x00, 0x01, 0x00, 0x48, 0x00};
+    unsigned char sign[4]    {'X', 'A', 'P', '1'};
+    unsigned char stride     {0x08};
+    unsigned char pad;
+    unsigned char count;     // number of files
+    unsigned char pad2;
+    struct
+    {
+        unsigned char filenum;
+        unsigned char id;
+        unsigned char pad[6];
+        unsigned int  begSec;
+        unsigned int  endSec;
+    } file[128];
+    unsigned char crc[272]; // I know it should be edc[4] and ecc[276], but this way is easier to handle
+};
+
+class XAPinterleaver : public interleaver
+{
+    void nullCustomizer(unsigned char *emptyBuffer, FileInfo &entry) override
+    {
+        emptyBuffer[0x010] = emptyBuffer[0x014] = entry.filenum.value_or(emptyBuffer[0x014]);
+        emptyBuffer[0x011] = emptyBuffer[0x015] = entry.nullTermination >= 0 ? entry.channel.value_or(0) : 0;
+        emptyBuffer[0x012] = emptyBuffer[0x016] = 0x48;
+    }
+public:
+    XAPinterleaver(const std::filesystem::path &path, const int &stride) : interleaver(path, stride) {}
+};
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -22,63 +56,66 @@ int main(int argc, char *argv[])
 
     if (strcasecmp(inputPath.extension().string().c_str(), ".xap"))
     {
-        class XAPinterleaver : public interleaver
-        {
-            void nullCustomizer(unsigned char *emptyBuffer, FileInfo &entry) override
-            {
-                emptyBuffer[0x010] = emptyBuffer[0x014] = entry.filenum.value_or(emptyBuffer[0x014]);
-                emptyBuffer[0x011] = emptyBuffer[0x015] = entry.nullTermination >= 0 ? entry.channel.value_or(0) : 0;
-                emptyBuffer[0x012] = emptyBuffer[0x016] = 0x48;
-            }
-        public:
-            XAPinterleaver(const std::filesystem::path &path, const int &stride) : interleaver(path, stride) {}
-        } files(inputPath, sectorStride);
-
+        XAPinterleaver files(inputPath, sectorStride);
         if (files.entries.empty())
         {
             fprintf(stderr, "Invalid manifest\n");
             return EXIT_FAILURE;
         }
 
+        const std::filesystem::path outputFile = argc >= 4 ? argv[3] : inputPath.stem() += "_NEW.XAP";
+        std::fstream output(outputFile, std::ios::binary | std::ios::in | std::ios::out | std::ios::trunc);
+        if (!output)
+        {
+            fprintf(stderr, "Error: Cannot write output file.\n");
+            return EXIT_FAILURE;
+        }
+
         if (!sectorSize)
             sectorSize = files.entries.front().sectorSize;
 
-        unsigned char buffer[CD_SECTOR_SIZE] {0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x02,
-                                            0x01, 0x00, 0x48, 0x00, 0x01, 0x00, 0x48, 0x00, 'X', 'A', 'P', '1', sectorStride, 0x00, (uint8_t)files.entries.size()};
-        //memcpy(buffer + 0x1E, (size_t[]){files.entries.size()}, sizeof(short));
-        const std::filesystem::path outputFile = argc >= 4 ? argv[3] : inputPath.stem() += "_NEW.XAP";
-        std::fstream output(outputFile, std::ios::binary | std::ios::in | std::ios::out | std::ios::trunc);
-        const int offset = sectorSize - XA_DATA_SIZE;
-
-        const size_t headerSectors = 1 + (files.entries.size() / 129);
-        for (size_t i = 0; i <= headerSectors; ++i)
+        const size_t headerSectors = 2 + (files.entries.size() / 129);
+        for (size_t i = 0; i < headerSectors; ++i)
         {
-            output.write(reinterpret_cast<const char*>(buffer) + (CD_SECTOR_SIZE - sectorSize), sectorSize);
+            XAPheader header {};
             if (!i)
-                continue;
-
-            int startIndex = (i - 1) * 128;
-            int seekOffset = sectorSize * i + offset;
-            int limit = std::min(files.entries.size(), i * 128);
-            for (int j = startIndex; j < limit; ++j)
+                header.count = (uint8_t)files.entries.size();
+            else
             {
-                auto &entry = files.entries[j];
-                if (!(*entry.channel % 128))
-                    output.seekp(seekOffset + 0x08, std::ios::beg);
-                else
+                int startIndex = (i - 1) * 128;
+                int limit      = std::min(files.entries.size(), i * 128) - 1;
+                int limitIndex = limit - startIndex;
+                for (int j = startIndex; j <= limit; ++j)
                 {
-                    output.seekp(seekOffset + 0x10 * (*entry.channel - startIndex), std::ios::beg);
-                    unsigned char data[8] {*entry.filenum, (uint8_t)(*entry.channel - 1)};
-                    output.write(reinterpret_cast<const char*>(data), sizeof(data));
+                    auto &entry   = files.entries[j];
+                    entry.begSec += headerSectors;
+                   (entry.endSec += headerSectors) -= entry.nullTermination * sectorStride;
+
+                    if (!(*entry.channel % 128))
+                    {
+                        memcpy(&header.sign, &entry.begSec, sizeof(entry.begSec));
+                        memcpy(&header.stride, &entry.endSec, sizeof(entry.endSec));
+                    }
+                    else
+                    {
+                        int index = *entry.channel - 1 - startIndex;
+                        if (index < 0 || index >= limitIndex)
+                        {
+                            fprintf(stderr, "Error: Invalid channel number \"%d\" for file \"%s\"\n", *entry.channel, entry.filePath.filename().string().c_str());
+                            return EXIT_FAILURE;
+                        }
+                        header.file[index].filenum = *entry.filenum;
+                        header.file[index].id      = *entry.channel - 1;
+                        header.file[index].begSec  = entry.begSec;
+                        header.file[index].endSec  = entry.endSec;
+                    }
                 }
-                output.write(reinterpret_cast<const char*>(&(entry.begSec += headerSectors)), sizeof(entry.begSec));
-                output.write(reinterpret_cast<const char*>(&((entry.endSec += headerSectors) -= entry.nullTermination * sectorStride)), sizeof(entry.endSec));
+
+                header.file[limitIndex].filenum = *files.entries[limit].filenum;
+                header.file[limitIndex].id      = limit;
             }
 
-            output.seekp(seekOffset + 0x10 * (limit - startIndex), std::ios::beg);
-            output.put(*files.entries[limit - 1].filenum);
-            output.put(limit - 1);
-            output.seekp(0, std::ios::end);
+            output.write(reinterpret_cast<const char*>(&header) + (CD_SECTOR_SIZE - sectorSize), sectorSize);
         }
 
         files.interleave(output, sectorSize);
